fb9f6d2048b2c0c0407cae92d74c7e659ccf8f17
20120470 <haonguyencp22@gmail.com>
2022-10-02 16:55:05 +0700

    responsive, move mouse course and news

---
+++ handleBtn.js
@@ -0,0 +1,33 @@
+$(document).ready(function ($) {
+     $(".accordion__header").next().slideToggle("slow"); //Close all news
+
+     // move mouse the news
+     $(".side").sortable({
+          axis: "y",
+          handle: "h3",
+          stop: function (event, ui) {
+               ui.item.children("h3").triggerHandler("focusout");
+          },
+     });
+
+     // handle click news side
+     $(".accordion__header").on("click", function () {
+          $(this).next().slideToggle(200);
+          $(this).children("i").toggleClass("arrow_minimize arrow_maximize");
+          $(this).toggleClass("side__active");
+     });
+
+     // handle click course
+     $("li").on("click", function () {
+          if ($(this).hasClass("selected")) $(this).removeClass("selected");
+          else $(this).addClass("selected");
+     });
+     
+     // handle move course by mouse
+     $(".courses-all").sortable({
+          connectWith: ".courses-register",
+     });
+     $(".courses-register").sortable({
+          connectWith: ".courses-all",
+     });
+});
